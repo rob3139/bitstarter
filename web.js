61a7a4b9526532ae574b91c598f0fb7c7bea2913
Rob Percival <robpercival@cantab.net>
2013-07-10 07:26:22 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var file = fs.readFileSync("index.html", "utf8");
+
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
-});
+  response.send(file);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
