916782ae280ea78d04a0412f975a3e72ebcc62b8
Rob Percival <robpercival@cantab.net>
2013-07-10 07:28:42 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -7,6 +7,7 @@ var file = fs.readFileSync("index.html", "utf8");
 
 app.get('/', function(request, response) {
   response.send(file);
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
